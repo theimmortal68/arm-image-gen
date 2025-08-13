16c2f6b5874efd9c7290ae90bb0aa2c8d401ef04
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-13 17:53:13 -0400

    Update 70-udev-perms.sh

--- custopizer/custom.d/70-udev-perms.sh
+++ custopizer/custom.d/70-udev-perms.sh
@@ -2,8 +2,7 @@
 set -x
 set -e
 export LC_ALL=C
-source /common.sh
-install_cleanup_trap
+source /common.sh; install_cleanup_trap
 
 # Basic serial device access for Klipper MCUs
 cat >/etc/udev/rules.d/99-klipper-serial.rules <<'EOF'
