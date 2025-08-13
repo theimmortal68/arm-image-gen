48e27fc92a222d58a4a2e8edf8fad5f417b7716e
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-13 17:52:53 -0400

    Update 60-services.sh

--- custopizer/custom.d/60-services.sh
+++ custopizer/custom.d/60-services.sh
@@ -2,8 +2,7 @@
 set -x
 set -e
 export LC_ALL=C
-source /common.sh
-install_cleanup_trap
+source /common.sh; install_cleanup_trap
 
 # Enable core services (CustoPiZer blocks starting during build)
 for s in klipper.service moonraker.service crowsnest.service nginx.service; do
