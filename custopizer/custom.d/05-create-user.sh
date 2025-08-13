c7c170abebf61098ba18b70119e436e4bbbec1de
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-13 17:50:30 -0400

    Update 05-create-user.sh

--- custopizer/custom.d/05-create-user.sh
+++ custopizer/custom.d/05-create-user.sh
@@ -2,9 +2,7 @@
 set -x
 set -e
 export LC_ALL=C
-
-source /common.sh
-install_cleanup_trap
+source /common.sh; install_cleanup_trap
 
 # Prefer value from 00-detect-user.sh; if missing, auto-detect/fallback.
 if [ -f /etc/ks-user.conf ]; then
