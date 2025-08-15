e55be8f8fb7cb36d64a173f1f38a09f4124eb709
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-15 17:49:15 -0400

    Update 40-dfu-util.sh

--- custopizer/custom.d/40-dfu-util.sh
+++ custopizer/custom.d/40-dfu-util.sh
@@ -4,35 +4,39 @@ export LC_ALL=C
 
 source /common.sh; install_cleanup_trap
 
-# dfu-util from source
-# Primary: https://gitlab.com/dfu-util/dfu-util.git
-# Fallbacks: git://git.code.sf.net/p/dfu-util/dfu-util  (or https://git.code.sf.net/p/dfu-util/dfu-util)
+# Build dfu-util from git (GitLab preferred; fall back to SourceForge)
+# Upstream needs autotools when cloning from git: autoconf, automake (aclocal), libtool, pkg-config, compiler.
+# You also asked for pandoc and libusb-1.0-0-dev.
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y --no-install-recommends \
-  libusb-1.0-0-dev autoconf pandoc
-
-# NOTE: If your base image doesn't already have a toolchain for autoconf builds,
-# you may need these too (uncomment if required):
-# apt-get install -y --no-install-recommends build-essential automake libtool pkg-config
+  git ca-certificates curl \
+  libusb-1.0-0-dev \
+  autoconf automake libtool pkg-config build-essential \
+  pandoc
 
 install -d -m 0755 /usr/local/src
 cd /usr/local/src
 
-# Clone with fallbacks
-rm -rf dfu-util
+# Honor "no idempotence": fail if the dir already exists
+if [ -e dfu-util ]; then
+  echo_red "[dfu-util] /usr/local/src/dfu-util already exists"; exit 1
+fi
+
+# Clone with fallbacks (network fallback only; still fails if dir exists)
 if git clone --depth=1 https://gitlab.com/dfu-util/dfu-util.git dfu-util; then
-  :
+  : # ok
 elif git clone --depth=1 git://git.code.sf.net/p/dfu-util/dfu-util dfu-util; then
-  :
+  : # ok
 else
   git clone --depth=1 https://git.code.sf.net/p/dfu-util/dfu-util dfu-util
 fi
 
 cd dfu-util
 
-# Autotools build
+# Autotools bootstrap + configure + build + install
+# (Using autogen.sh, which runs autoreconf -i under the hood.)
 ./autogen.sh
 ./configure --prefix=/usr/local
 make -j"$(nproc)"
@@ -43,3 +47,5 @@ if command -v dfu-util >/dev/null 2>&1; then
   install -d -m 0755 /etc
   printf 'DFU-UTIL\t%s\n' "$(dfu-util --version | head -n1)" >> /etc/ks-manifest.txt
 fi
+
+echo_green "[dfu-util] built and installed from git"
